bc50dc9a102a671fc37e3225894d6da8df8a05a8
Tom Martin <tom@spaceapegames.com>
2014-11-29 11:24:23 +0000

    snow mesh

--- js/snow.js
+++ js/snow.js
@@ -1,15 +1,34 @@
 function Snow(scene) { 
 
-	var geom = new THREE.Geometry();
-	var faceIndex = 0;
+    var perturb = function(factor) {
+        return factor-(Math.random()*(factor*2));
+    }
+    var xMax = 100;
+    var zMax = 100;
 
-	geom.vertices.push(new THREE.Vector3(0, 0, 0));
-	geom.vertices.push(new THREE.Vector3(0, 0, 1));
-	geom.vertices.push(new THREE.Vector3(1, 0, 1));
+    var geom = new THREE.Geometry();
+    var vertIndex = 0;
 
-	geom.faces.push( new THREE.Face3( 0, 1, 2));
+    var z = -(zMax/2);
+    for(var zIndex = 0; zIndex < zMax; zIndex+=1) {
+        var x = -(xMax/2);
+        for(var xIndex = 0; xIndex < xMax; xIndex+=1) {
 
-	var material = new THREE.MeshLambertMaterial( { color: 0xffffff} );
+            geom.vertices.push(new THREE.Vector3(x+perturb(0.25), perturb(0.1), z+perturb(0.25)));
+            
+            if(zIndex > 0 && xIndex > 0) {
+                geom.faces.push( new THREE.Face3( (vertIndex-xMax)-1, vertIndex-1, vertIndex-xMax));
+                geom.faces.push( new THREE.Face3( vertIndex-xMax, vertIndex-1, vertIndex));
+            }
+            vertIndex+=1;
+            x += 1
+        }
+        z +=1;
+    }
+
+    geom.computeFaceNormals();
+
+    var material = new THREE.MeshLambertMaterial( { color: 0xffffff} );
     this.mesh = new THREE.Mesh( geom, material );
     scene.add(this.mesh);
 }
